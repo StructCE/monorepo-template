289d68e10447e11757e707683135e7868eecae14
artistrea <apiravesi@gmail.com>
2023-06-11 15:59:10 -0300

    working stuff

--- packages/auth/src/auth-options.ts
+++ packages/auth/src/auth-options.ts
@@ -1,7 +1,8 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { prisma } from "@struct/db";
 import { type DefaultSession, type NextAuthOptions } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
+
+import { prisma } from "@struct/db";
 
 import { env } from "../env.mjs";
 
