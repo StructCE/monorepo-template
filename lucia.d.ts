41d7e7b2bef4fa797b276ecb0660404581c48cdf
artistrea <apiravesi@gmail.com>
2023-08-01 12:22:37 -0300

    refactor: global types dir

--- lucia.d.ts
+++
@@ -1,7 +0,0 @@
-// lucia.d.ts
-/// <reference types="lucia" />
-declare namespace Lucia {
-  type Auth = import("./packages/api/lucia").Auth;
-  type DatabaseUserAttributes = Omit<import("./packages/db").User, "id">;
-  type DatabaseSessionAttributes = {};
-}
