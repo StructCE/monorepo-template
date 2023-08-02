21c42d644102930bf9a88a01ca461a09056e7acf
artistrea <apiravesi@gmail.com>
2023-08-02 13:13:35 -0300

    fix: email plain text also available

--- packages/email-templates/index.ts
+++ packages/email-templates/index.ts
@@ -2,7 +2,7 @@ import { render } from "@react-email/render";
 
 import ConfirmSignUp from "./src/emails/ConfirmSignUp";
 
-export function getConfirmSignUpHTML({
+export function getConfirmSignUpEmail({
   confirmationLink,
   email,
   logoUrl,
@@ -11,11 +11,26 @@ export function getConfirmSignUpHTML({
   email: string;
   logoUrl: string;
 }) {
-  return render(
+  const html = render(
     ConfirmSignUp({
       confirmationLink,
       email,
       logoUrl,
     }),
   );
+
+  // some email clients can't render html, so we also include a plain text version
+  const text = render(
+    ConfirmSignUp({
+      confirmationLink,
+      email,
+      logoUrl,
+    }),
+    { plainText: true },
+  );
+
+  return {
+    html,
+    text,
+  };
 }
