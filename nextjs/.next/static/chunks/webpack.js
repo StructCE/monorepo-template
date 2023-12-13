754dd3ca921954e3c01facfe44f6496d83c87e6b
Willyan Marques <willyanmarquesmelo@gmail.com>
2023-12-13 16:36:25 -0300

    Owner model no schema CRUD inicial página registro (testar)

--- nextjs/.next/static/chunks/webpack.js
+++ nextjs/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4c3252111480164d"; }
+/******/ 		__webpack_require__.h = function() { return "1d4f79fba40474bd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
